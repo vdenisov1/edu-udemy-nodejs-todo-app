d0cd85a2e0eb8239cbbeea1b20e29daecc1be797
Vlad Denisov <vlad@vdenisov.net>
2018-12-08 18:03:25 -0500

    Added GET /todos/:id routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/Todo');
@@ -25,14 +26,31 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     
-    Todo.find().then((todos) => {
+    Todo.find().then((items) => {
         res.status(200);
-        res.send({ todos });
+        res.send({ items });
     }, (err) => {
         res.status(400);
         res.send(err);
     });
 
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(req.params.id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }, (err) => {
+        res.status(400).send();
+    });
 });
 
 // app.get('/todos', (req, res) => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,25 @@
 const request = require('supertest');
 const expect = require('expect.js');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/Todo');
 
+const createdTodos = [
+  {
+    _id: new ObjectID(),
+    text: "First test todo"
+  },
+  {
+    _id: new ObjectID(),
+    text: "Second test todo"
+  }
+];
+
 beforeEach((done) => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(createdTodos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,8 +40,8 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).to.be(1);
-                    expect(todos[0].text).to.be(text);
+                    expect(todos.length).to.be(createdTodos.length + 1);
+                    expect(todos[createdTodos.length].text).to.be(text);
                     done();
                 }).catch((err) => done(err));
             });
@@ -49,7 +63,7 @@ describe('POST /todos', () => {
                     }
 
                     Todo.find().then((todos) => {
-                        expect(todos.length).to.be(0);
+                        expect(todos.length).to.be(createdTodos.length);
                         done();
                     }).catch((err) => done(err));
                 });
@@ -61,23 +75,68 @@ describe('POST /todos', () => {
 describe('GET /todos', () => {
 
     it('should get a list of todos', (done) => {
-        let text = 'Make this test pass';
-        let todoItem = new Todo({ text });
-        
-        todoItem.save().then((doc) => {
-            request(app)
-                .get('/todos')
-                .expect(200)
-                .end((err, res) => {
-                    if(err) {
-                        return done(err);
-                    }
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
 
-                    expect(res.body.todos.length).to.be(1);
-                    expect(res.body.todos[0].text).to.be(text);
-                    done();
-                });
-        }).catch((err) => done(err));
+                expect(res.body.items.length).to.be(createdTodos.length);
+                expect(res.body.items[0].text).to.be(createdTodos[0].text);
+                done();
+            });
+    });
+
+});
+
+describe('GET /todos/:id', () => {
+
+    it('should get back a todo item', (done) => {
+        let item = createdTodos[0];
+        request(app)
+            .get(`/todos/${createdTodos[0]._id}`)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                expect(res.body).to.have.property("_id");
+                expect(res.body._id).to.be(item._id.toString());
+                expect(res.body.text).to.be(item.text);
+                expect(res.body.completed).to.be(false);
+                done();
+            });
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get(`/todos/abcd`)
+            .expect(404)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                
+                done();
+            });
+    });
+
+    it('should return 404 if object not found ids', (done) => {
+        let newId = new ObjectID();
+
+        request(app)
+            .get(`/todos/${newId}`)
+            .expect(404)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                done();
+            });
     });
 
 });
