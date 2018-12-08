6ade960c168fc52fc40794248bef16f0968c4691
Vlad Denisov <vlad@vdenisov.net>
2018-12-08 16:53:38 -0500

    Created the todos endpoint

---
+++ server/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/Todo');
+const { User } = require('./models/User');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todoItem = new Todo(req.body);
+
+    todoItem.save().then((doc) => {
+        res.status(204);
+        res.send(doc);
+    }, (err) => { 
+        res.status(400);
+        res.send(err);
+    });
+});
+
+// app.get('/todos', (req, res) => {
+
+// });
+
+// app.get('/todos/:id', (req, res) => {
+
+// });
+
+app.listen(3000, () => {
+    console.log('Started on port', 3000);
+});
+
+module.exports = { app };
